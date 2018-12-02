43667f0ec1ee39442ed289ef5d7289e8aa24cd4b
b05902109 <b05902109@ntu.edu.tw>
2018-12-03 01:49:41 +0800

    fix pipe4

--- code/MEMWB_pipeline.v
+++ code/MEMWB_pipeline.v
@@ -17,7 +17,7 @@ input	[31:0]	data_i, ALUdata_i, inst_i;
 output	reg				WB_o;
 output	reg 	[31:0]	data_o, ALUdata_o, inst_o;
 
-always @(posedge clk_i or negedge clk_i) begin
+always @(posedge clk_i) begin
 	WB_o = WB_i;
 	data_o = data_i;
 	ALUdata_o = ALUdata_il;
