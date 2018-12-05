666188379390b99f0d3dc6dd1b8bf13907169cde
b05902109 <b05902109@ntu.edu.tw>
2018-12-05 21:49:25 +0800

    done!

--- code/run.sh
+++ code/run.sh
@@ -1,2 +1,3 @@
-iverilog -o myCPU testbench.v CPU.v Add_Branch.v Add_PC.v ALU_Control.v ALU.v Control.v Data_Memory.v ForwardingUnit.v HazzardDetection.v ImmGen.v Instruction_Memory.v MUX_ALU1.v MUX_ALU2.v MUX_ALUSrc.v MUX_EXMEM1.v MUX_EXMEM2.v MUX_IDEX.v MUX_PC.v MUX_WB.v PC.v Pipeline_IFID.v Pipeline_IDEX.v Pipeline_EXMEM.v Pipeline_MEMWB.v Registers.v
+iverilog -o myCPU testbench.v CPU.v Add_Branch.v Add_PC.v ALU_Control.v ALU.v Control.v Data_Memory.v ForwardingUnit.v HazzardDetection.v ImmGen.v Instruction_Memory.v MUX_ALU1.v MUX_ALU2.v MUX_ALUSrc.v MUX_IDEX.v MUX_PC.v MUX_WB.v PC.v Pipeline_IFID.v Pipeline_IDEX.v Pipeline_EXMEM.v Pipeline_MEMWB.v Registers.v
+#iverilog -o myCPU *.v
 #vvp myCPU
